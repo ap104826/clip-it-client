ec2fecf1c8f7d1809bac287ecc9668c23f3b5b0a
Arpita Mehta <arpita.patel02@gmail.com>
2020-06-26 10:43:27 -0700

    added tests

--- src/AddCategory/AddCategory.test.js
+++ src/AddCategory/AddCategory.test.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import renderer from 'react-test-renderer';
-import Category from './Category';
-import { BrowserRouter as BR } from 'react-router-dom';
+import toJson from 'enzyme-to-json'
+import { shallow } from 'enzyme'
+import AddCategory from './AddCategory';
 
 
-describe('Category component', () => {
-  const props = {
-    category: [{
-      name: 'some name',
-      id: 'id',
-    }]
-  }
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-
-    ReactDOM.render(<BR><Category {...props} /></BR>, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
-
-  it('renders the UI as expected', () => {
-    const category = renderer.create(<BR><Category {...props} /></BR>);
-
-    expect(category.toJSON()).toMatchSnapshot();
-  });
+describe(`AddCategory component`, () => {
+  it('renders the complete form', () => {
+    const wrapper = shallow(<AddCategory />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+})
